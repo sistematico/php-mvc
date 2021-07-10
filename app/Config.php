ed378465c1f511e4f819a4a75ecf2c7e40e45812
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-10 18:38:01 -0400

    Autocommit por GitHub: alterações: add 'app/Config.php'

---
+++ app/Config.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+/**
+ * Application configuration
+ *
+ * PHP version 7.0
+ */
+class Config
+{
+
+    /**
+     * Database file
+     * @var string
+     */
+    const DB_FILE = '../data/database.sqlite';
+
+    /**
+     * Show or hide error messages on screen
+     * @var boolean
+     */
+    const SHOW_ERRORS = true;
+}
