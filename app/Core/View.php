5d736d328fc718110953110682d55e949961756a
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-10 19:58:01 -0400

    Autocommit por GitHub: alterações: add 'app/Core/View.php'

--- app/Core/View.php
+++ app/Core/View.php
@@ -6,12 +6,13 @@ class View
 {
     private static function content($view)
     {
-        $file = __DIR__ . '/../view/{$view}.html';
+        $file = __DIR__ . '/../../view/{$view}.html';
         return file_exists($file) ? file_get_contents($file) : '';
     }
 
     public static function render($view, $vars = [])
     {
-        return self::content($view);
+        $content = self::content($view);
+        return $content;
     }
 }
