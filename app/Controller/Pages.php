3f012de26ccaf6d96e3d1d9491e395c65e22cc01
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-11 14:40:01 -0400

    Autocommit por GitHub: alterações: add 'app/Controller/Pages.php' add 'app/Core/View.php'

--- app/Controller/Pages.php
+++ app/Controller/Pages.php
@@ -24,7 +24,6 @@ class Pages extends View
     public static function getAdminLogin($request, $message = null)
     {
         $status = !is_null($message) ? $message : '';
-
         return parent::adminLogin('admin/login', 'Login', ['status' => $status]);
     }
     
