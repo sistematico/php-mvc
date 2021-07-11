7947666242d075d52f2b4dad6c3e8a57b8c9833a
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-11 01:34:01 -0400

    Autocommit por GitHub: alterações: add 'app/Core/Database.php' add 'app/Core/Pagination.php'

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -50,51 +50,36 @@ class Database
     $fields = array_keys($values);
     $binds  = array_pad([],count($fields),'?');
 
-    //MONTA A QUERY
     $query = 'INSERT INTO '.$this->table.' ('.implode(',',$fields).') VALUES ('.implode(',',$binds).')';
 
-    //EXECUTA O INSERT
     $this->execute($query,array_values($values));
 
-    //RETORNA O ID INSERIDO
     return $this->connection->lastInsertId();
   }
 
   public function select($where = null, $order = null, $limit = null, $fields = '*'){
-    //DADOS DA QUERY
     $where = strlen($where) ? 'WHERE '.$where : '';
     $order = strlen($order) ? 'ORDER BY '.$order : '';
     $limit = strlen($limit) ? 'LIMIT '.$limit : '';
 
-    //MONTA A QUERY
     $query = 'SELECT '.$fields.' FROM '.$this->table.' '.$where.' '.$order.' '.$limit;
-
-    //EXECUTA A QUERY
     return $this->execute($query);
   }
 
   public function update($where,$values){
-    //DADOS DA QUERY
     $fields = array_keys($values);
-
-    //MONTA A QUERY
     $query = 'UPDATE '.$this->table.' SET '.implode('=?,',$fields).'=? WHERE '.$where;
 
-    //EXECUTAR A QUERY
     $this->execute($query,array_values($values));
 
-    //RETORNA SUCESSO
     return true;
   }
 
   public function delete($where){
-    //MONTA A QUERY
     $query = 'DELETE FROM '.$this->table.' WHERE '.$where;
 
-    //EXECUTA A QUERY
     $this->execute($query);
 
-    //RETORNA SUCESSO
     return true;
   }
 }
