c5abab7f6e30a859ca7fd83dc95feed7879f1213
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-11 15:08:02 -0400

    Autocommit por GitHub: alterações: add 'app/Core/Session.php'

--- app/Core/Session.php
+++ app/Core/Session.php
@@ -4,8 +4,24 @@ namespace App\Core;
 
 class Session
 {
-   public static function adminLogin($user)
-   {
-       
-   }
+
+    private static function init()
+    {
+        if (session_status() != PHP_SESSION_ACTIVE) {
+            session_start();
+        }
+    }
+
+    public static function adminLogin($user)
+    {
+        self::init();
+
+        $_SESSION['admin']['user'] = [
+            'id'       => $user->id,
+            'login'    => $user->login,
+            'email'    => $user->email,
+            'fullname' => $user->fullname,
+        ];
+
+    }
 }
