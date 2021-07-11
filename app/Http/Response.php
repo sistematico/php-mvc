d9d729fb8f7c006067a3b745a4785a135ef58d17
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-10 21:12:01 -0400

    Autocommit por GitHub: alterações: add 'app/Http/Response.php'

--- app/Http/Response.php
+++ app/Http/Response.php
@@ -7,4 +7,13 @@ class Response
     private $httpCode = 200;
     private $headers = [];
     private $contentType = 'text/html';
+    private $content;
+
+    public function __construct($httpCode, $content, $contentType = 'text/html')
+    {
+        $this->httpCode = $httpCode;
+        $this->content = $content;
+        $this->httpCode = $httpCode;
+        $this->httpCode = $httpCode;
+    }
 }
