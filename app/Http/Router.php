94938b1cfcc99565fdffe5ae71f80f431ddd77fd
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-10 21:36:01 -0400

    Autocommit por GitHub: alterações: add 'app/Http/Router.php'

---
+++ app/Http/Router.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http;
+
+class Router
+{
+    private $url = '';
+    private $prefix = '';
+    private $routes = [];
+    private $request;
+    
+    public function __construct($url)
+    {
+        $this->request = new Request();
+        $this->url = $url;
+    }
+}
