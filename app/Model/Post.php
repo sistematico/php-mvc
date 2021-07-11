a2c89ffd9f7c044645935b98f305cef39367535d
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-11 02:00:01 -0400

    Autocommit por GitHub: alterações: add 'app/Model/Post.php'

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -2,10 +2,24 @@
 
 namespace App\Model;
 
+use App\Core\Database;
+
 class Post
 {
     public $id;
     public $name;
     public $description;
     public $picture;
+    public $date;
+
+    public function create()
+    {
+        $this->id = (new Database('posts'))->insert([
+            'title' => $this->title,
+            'description' => $this->description,
+            'picture' => $this->picture
+        ]);
+
+        return true;
+    }
 }
