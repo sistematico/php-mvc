9a526342779aefa59497bce935c7adb318ae3b80
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-10 21:02:01 -0400

    Autocommit por GitHub: alterações: add 'app/Http/Request.php' add 'app/Http/Response.php'

--- app/Http/Request.php
+++ app/Http/Request.php
@@ -19,4 +19,30 @@ class Request
         $this->uri = $_SERVER['REQUEST_URI'] ?? '';
     }
 
+
+    public function getHttpMethod()
+    {
+        return $this->httpMethod;
+    }
+
+    public function getUri()
+    {
+        return $this->uri;
+    }
+    
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+    
+    public function getQueryParams()
+    {
+        return $this->queryParams;
+    }
+    
+    public function getPostVars()
+    {
+        return $this->postVars;
+    }
+
 }
