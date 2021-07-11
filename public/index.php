041d67b7c39a914306a2e073815296804ce52f2e
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-10 21:38:02 -0400

    Autocommit por GitHub: alterações: add 'public/index.php'

--- public/index.php
+++ public/index.php
@@ -2,6 +2,14 @@
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
+use App\Http\Router;
 use App\Controller\Pages;
+
+$router = new Router('');
+
+echo '<pre>';
+print_r($router);
+echo '</pre>';
+exit;
 
 echo Pages::home();
