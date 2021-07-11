56c97a09bd6a2b708da536110ecb0926a5597a9d
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-11 11:06:01 -0400

    Autocommit por GitHub: alterações: add 'app/Controller/Posts.php' add 'app/view/posts/post.html'

--- app/Controller/Posts.php
+++ app/Controller/Posts.php
@@ -25,7 +25,7 @@ class Posts extends View
             $items .= parent::render('posts/post',[
                 'title' => $row->title,
                 'description' => $row->description,
-                'date' => $row->date,
+                'date' => $row->created,
                 'likes' => $row->likes
             ]);
         }
