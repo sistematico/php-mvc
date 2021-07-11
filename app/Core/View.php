16a0a8445390a3ceeaff2b6305d116e17839727e
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-11 00:20:01 -0400

    Autocommit por GitHub: alterações: add 'app/Core/View.php'

--- app/Core/View.php
+++ app/Core/View.php
@@ -11,13 +11,13 @@ class View
         self::$vars = $vars;
     }
 
-    private static function content($dir, $view)
+    private static function content($view)
     {
-        $file = dirname(__DIR__) . "/view/{$dir}/{$view}.html";
+        $file = dirname(__DIR__) . "/view/{$view}.html";
         return file_exists($file) ? file_get_contents($file) : '';
     }
 
-    protected static function render($dir, $view, $vars = [])
+    protected static function render($view, $vars = [])
     {
         $vars = array_merge(self::$vars, $vars);
 
@@ -25,14 +25,14 @@ class View
             return '{{' . $item . '}}'; 
         },array_keys($vars));
 
-        return str_replace($keys, array_values($vars),self::content($dir, $view));
+        return str_replace($keys, array_values($vars),self::content($view));
     }
 
     protected static function page($dir, $view, $title, $vars = [])
     {
-        return View::render('main', [
-            'header' => self::render('header'),
-            'footer' => self::render('footer'),
+        return View::render($dir . '/' . $view, [
+            'header' => self::render($dir . '/header'),
+            'footer' => self::render($dir . '/footer'),
             'title' => $title,
             'content' => self::render($view, $vars)
         ]);
