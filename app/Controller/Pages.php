3b809ea8702269945af85e9d917ddf1597007017
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-11 00:16:01 -0400

    Autocommit por GitHub: alterações: add 'app/Controller/Pages.php'

--- app/Controller/Pages.php
+++ app/Controller/Pages.php
@@ -8,14 +8,14 @@ class Pages extends View
 {
     public static function home()
     {
-        return parent::main('home', 'Home', [
+        return parent::page('main', 'home', 'Home', [
             'name' => 'PHP2 MVC'
         ]);
     }
 
     public static function posts()
     {
-        return parent::main('posts/main', 'Home', [
+        return parent::page('posts', 'main', 'Home', [
             'name' => 'PHP2 MVC'
         ]);
     }
