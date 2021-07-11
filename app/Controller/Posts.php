320321d715c45dbc1818baf6dab46d1c70e9f112
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-11 02:48:01 -0400

    Autocommit por GitHub: alterações: add 'app/Controller/Posts.php'

--- app/Controller/Posts.php
+++ app/Controller/Posts.php
@@ -9,7 +9,8 @@ class Posts extends View
 {
     public static function getPosts()
     {
-        return Post::getPosts(null, 'id DESC');
+        //return Post::getPosts(null, 'id DESC');
+        return parent::page('posts', 'Posts');
     }
 
     public static function getPost($request)
@@ -20,14 +21,13 @@ class Posts extends View
     public static function insertPost($request)
     {
         $postVars = $request->getPostVars();
+
         $post = new Post;
         $post->title = $postVars['title'];
         $post->description = $postVars['description'];
         $post->picture = $postVars['picture'];
         $post->create();
 
-        return parent::page('posts', 'Posts', [
-            'name' => 'PHP2 MVC'
-        ]);
+        return self::getPosts();
     }
 }
