3a419262c3cbd9812a5f35224a5becba35c74258
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-11 01:32:01 -0400

    Autocommit por GitHub: alterações: add 'app/Core/Database.php'

---
+++ app/Core/Database.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Core;
+
+use \PDO;
+use \PDOException;
+
+class Database
+{
+  private $table;
+  private $connection;
+  private static $driver;
+  private static $path;
+
+  public static function config($path, $driver = 'sqlite')
+  {
+    self::$driver = $driver;
+    self::$path = $path;
+  }
+
+  public function __construct($table = null)
+  {
+    $this->table = $table;
+    $this->setConnection();
+  }
+
+  private function setConnection()
+  {
+    try{
+      $this->connection = new PDO(self::$driver . ':' . self::$path);
+      $this->connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    }catch(PDOException $e){
+      die('ERROR: '.$e->getMessage());
+    }
+  }
+
+  public function execute($query,$params = [])
+  {
+    try{
+      $statement = $this->connection->prepare($query);
+      $statement->execute($params);
+      return $statement;
+    }catch(PDOException $e){
+      die('ERROR: '.$e->getMessage());
+    }
+  }
+
+  public function insert($values)
+  {
+    $fields = array_keys($values);
+    $binds  = array_pad([],count($fields),'?');
+
+    //MONTA A QUERY
+    $query = 'INSERT INTO '.$this->table.' ('.implode(',',$fields).') VALUES ('.implode(',',$binds).')';
+
+    //EXECUTA O INSERT
+    $this->execute($query,array_values($values));
+
+    //RETORNA O ID INSERIDO
+    return $this->connection->lastInsertId();
+  }
+
+  public function select($where = null, $order = null, $limit = null, $fields = '*'){
+    //DADOS DA QUERY
+    $where = strlen($where) ? 'WHERE '.$where : '';
+    $order = strlen($order) ? 'ORDER BY '.$order : '';
+    $limit = strlen($limit) ? 'LIMIT '.$limit : '';
+
+    //MONTA A QUERY
+    $query = 'SELECT '.$fields.' FROM '.$this->table.' '.$where.' '.$order.' '.$limit;
+
+    //EXECUTA A QUERY
+    return $this->execute($query);
+  }
+
+  public function update($where,$values){
+    //DADOS DA QUERY
+    $fields = array_keys($values);
+
+    //MONTA A QUERY
+    $query = 'UPDATE '.$this->table.' SET '.implode('=?,',$fields).'=? WHERE '.$where;
+
+    //EXECUTAR A QUERY
+    $this->execute($query,array_values($values));
+
+    //RETORNA SUCESSO
+    return true;
+  }
+
+  public function delete($where){
+    //MONTA A QUERY
+    $query = 'DELETE FROM '.$this->table.' WHERE '.$where;
+
+    //EXECUTA A QUERY
+    $this->execute($query);
+
+    //RETORNA SUCESSO
+    return true;
+  }
+}
