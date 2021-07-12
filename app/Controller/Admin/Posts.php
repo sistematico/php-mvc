fce0814217c15998d6397d58f303bbb7e43a6a52
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-12 11:54:01 -0400

    Autocommit por GitHub: alterações: add 'app/Controller/Admin/Posts.php'

--- app/Controller/Admin/Posts.php
+++ app/Controller/Admin/Posts.php
@@ -2,16 +2,46 @@
 
 namespace App\Controller\Admin;
 
-// use \App\Controller\Admin\Page;
 use \App\Core\View;
+use \App\Model\Post;
+use \App\Core\Pagination;
 
 class Posts extends Page
 {
 
-    public static function getPosts($request)
+    private static function getPostItems($request, &$pagination)
     {
-        $content = View::render('admin/posts/index', []);
-        return parent::getAdminPanel('Posts', $content, 'posts');
+        $items = '';
+        
+        $total = Post::getPosts(null, null, null, 'COUNT(*) as total')->fetchObject()->total;
+        
+        $queryParams = $request->getQueryParams();
+        $current = $queryParams['pagina'] ?? 1;
+
+        $pagination = new Pagination($total, $current, 1);
+
+        $results = Post::getPosts(null, 'id DESC', $pagination->getLimit());
+
+        while ($row = $results->fetchObject(Post::class)) {
+            $items .= parent::render('posts/post',[
+                'title' => $row->title,
+                'description' => $row->description,
+                'picture' => $row->picture,
+                'likes' => $row->likes,
+                'created' => date('d/m/Y H:i:s', strtotime($row->created)),
+                'updated' => date('d/m/Y H:i:s', strtotime($row->updated))
+            ]);
+        }
+
+        return $items;
     }
 
+    public static function getPosts($request)
+    {
+        $content = View::render('admin/posts/index', [
+            'items' => self::getPostItems($request, $pagination)
+        ]);
+
+        return parent::getAdminPanel('Posts', $content, 'posts');
+    }
 }
