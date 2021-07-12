a4d81daa01f3644ae534e7c1b0e5fa2084367b60
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-11 21:34:01 -0400

    Autocommit por GitHub: alterações: add 'app/bootstrap.php'

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -9,7 +9,9 @@
 \App\Http\Middleware\Queue::setMap([
     'maintenance' => \App\Http\Middleware\Maintenance::class,
     'admin-login' => \App\Http\Middleware\AdminLogin::class,
-    'admin-logout' => \App\Http\Middleware\AdminLogout::class
+    'admin-logout' => \App\Http\Middleware\AdminLogout::class,
+    'user-login' => \App\Http\Middleware\UserLogin::class,
+    'user-logout' => \App\Http\Middleware\UserLogout::class
 ]);
 
 \App\Http\Middleware\Queue::setDefault(['maintenance']);
