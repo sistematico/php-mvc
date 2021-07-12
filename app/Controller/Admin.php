1749868ade51a1e966df174a58e277bf3984c570
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-12 09:42:02 -0400

    Autocommit por GitHub: alterações: add 'app/Controller/Admin.php'

--- app/Controller/Admin.php
+++ app/Controller/Admin.php
@@ -17,33 +17,9 @@ class Admin extends View
         'posts' => ['label' => 'Posts',    'link' => URL . '/admin/posts', 'icon' => 'file']
     ];
 
-    private static function getPostItems($request, &$pagination)
-    {
-        $items = '';
-        
-        $total = Post::getPosts(null, null, null, 'COUNT(*) as total')->fetchObject()->total;
-        
-        $queryParams = $request->getQueryParams();
-        $current = $queryParams['pagina'] ?? 1;
+    
 
-        $pagination = new \App\Core\Pagination($total, $current, 1);
-
-        $results = Post::getPosts(null, 'id DESC', $pagination->getLimit());
-
-        while ($row = $results->fetchObject(Post::class)) {
-            $items .= parent::render('posts/post',[
-                'title' => $row->title,
-                'description' => $row->description,
-                'picture' => $row->picture,
-                'likes' => $row->likes,
-                'created' => date('d/m/Y H:i:s', strtotime($row->created)),
-                'updated' => date('d/m/Y H:i:s', strtotime($row->updated))
-            ]);
-        }
-
-        return $items;
-    }
-
+    // Login
     public static function getLogin($request, $message = null)
     {
         $alert =  !is_null($message) ? parent::render('admin/alert', ['status' => $message]) : '';
@@ -76,45 +52,5 @@ class Admin extends View
     {
         Session::adminLogout();
         $request->getRouter()->redirect('/admin/login');
-    }
-
-    public static function getPosts($request)
-    {
-        $content = parent::render('admin/posts', 'Posts', [
-            'items' => self::getPostItems($request, $pagination),
-            'pagination' => parent::getPagination($request, $pagination)
-        ]);
-        
-        return self::getPage('Cadastrar depoimento', $content, 'posts');
-    }
-
-    public static function getPanel($title, $content, $current)
-    {
-        $links = '';
-        foreach (self::$links as $hash => $item) {
-            $links .= parent::render('admin/menu/link', [
-                'label' => $item['label'],
-                'link' => $item['link'],
-                'icon' => $item['icon'],
-                'current' => $hash == $current ? 'active' : '',
-            ]);
-        }
-
-        $contentPanel = [
-            'title' => 'Painel de Admin',
-            'links' => $links
-        ];
-
-        return self::getPage('admin/dashboard', $title, $contentPanel);
-    }   
-
-    private static function getPage($view, $title, $vars = [])
-    {
-        return parent::render('admin/main', [
-            'header' => parent::render('admin/header'),
-            'footer' => parent::render('admin/footer'),
-            'content' => parent::render($view, $vars),
-            'title' => $title
-        ]);
     }
 }
