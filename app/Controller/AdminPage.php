3a609e39bd6a02fcc67fa5bccc5696e67b0f2e02
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-12 09:56:01 -0400

    Autocommit por GitHub: alterações: add 'app/Controller/AdminPage.php'

--- app/Controller/AdminPage.php
+++ app/Controller/AdminPage.php
@@ -7,11 +7,17 @@ use \App\Core\View;
 class AdminPage extends View
 {
 
-    public static function getPage($title, $content)
+    public static function getAdminPage($title, $content)
     {
         return View::render('admin/page',[
-
+            'title' => $title,
+            'content' => $content
         ]);
+    }
+
+    public static function getAdminPanel($title, $content, $current)
+    {
+        return self::getAdminPage($title, $content);
     }
 
 }
