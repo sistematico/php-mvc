8df05b502a58fe3c32ba6f9f9c4dc7d376bd2d0c
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-11 22:56:01 -0400

    Autocommit por GitHub: alterações: add 'app/routes/admin.php'

--- app/routes/admin.php
+++ app/routes/admin.php
@@ -6,27 +6,41 @@ use App\Controller;
 $router->get('/admin', [
     'middlewares' => ['admin-login'],
     function($request) {
-        return new Response(200, Controller\Admin::getAdminPanel('Admin', '', 'home'));
+        return new Response(200, Controller\Admin::getPanel('Admin', '', 'home'));
     }
 ]);
 
 $router->get('/admin/login', [
     'middlewares' => ['admin-logout'],
     function($request) {
-        return new Response(200, Controller\Admin::getAdminLogin($request));
+        return new Response(200, Controller\Admin::getLogin($request));
     }
 ]);
 
 $router->post('/admin/login', [
     'middlewares' => ['admin-logout'],
     function($request) {
-        return new Response(200, Controller\Admin::setAdminLogin($request));
+        return new Response(200, Controller\Admin::setLogin($request));
     }
 ]);
 
 $router->get('/admin/logout', [
     'middlewares' => ['admin-login'],
     function($request) {
-        return new Response(200, Controller\Admin::setAdminLogout($request));
+        return new Response(200, Controller\Admin::setLogout($request));
+    }
+]);
+
+$router->get('/admin/posts', [
+    'middlewares' => ['admin-login'],
+    function($request) {
+        return new Response(200, Controller\Admin::getPanel('Admin', '', 'home'));
+    }
+]);
+
+$router->get('/admin/users', [
+    'middlewares' => ['admin-login'],
+    function($request) {
+        return new Response(200, Controller\Admin::getPanel('Admin', '', 'home'));
     }
 ]);
