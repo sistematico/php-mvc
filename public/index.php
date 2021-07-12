edc40545e1a1d7a7cf16073f8a4cbd3836a0663a
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-11 22:20:01 -0400

    Autocommit por GitHub: alterações: add 'public/index.php'

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 use App\Http\Router;
@@ -9,6 +10,18 @@ define('APP', ROOT . '/app');
 define('SITENAME', 'PHP MVC');
 define('URL', 'https://localhost');
 define('MAINTENANCE', false);
+
+$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+echo '<pre>';
+print_r($actual_link);
+echo '</pre>';
+exit;
+
+echo '<pre>';
+print_r(URL);
+echo '</pre>';
+exit;
 
 require APP . '/bootstrap.php';
 
