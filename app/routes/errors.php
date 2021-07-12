0b8ee3fb4fdf25b9d71ea69217be020739c36e67
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-12 12:06:01 -0400

    Autocommit por GitHub: alterações: add 'app/routes/errors.php' remove 'app/view/error/404.html' add 'app/view/error.html'

--- app/routes/errors.php
+++ app/routes/errors.php
@@ -5,12 +5,12 @@ use App\Controller;
 
 $router->get('/404', [
     function($request) {
-        return new Response(200, Controller\Pages::notFound($request));
+        return new Response(200, Controller\Pages::error(404, 'Not Found'));
     }
 ]);
 
 $router->get('/405', [
     function($request) {
-        return new Response(200, Controller\Pages::notAllowed($request));
+        return new Response(200, Controller\Pages::error(405, 'Not Allowed'));
     }
 ]);
