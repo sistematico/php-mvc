1d44eb6834aed151ff46203b882d854403f4b88d
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-11 20:30:01 -0400

    Autocommit por GitHub: alterações: add 'app/routes.php' add 'public/index.php'

--- app/routes.php
+++ app/routes.php
@@ -1,15 +1,55 @@
 <?php
 
-\App\Core\View::init([
-    'SITENAME'=>SITENAME,
-    'URL'=>URL,
-    'YEAR'=> date('Y')
+use App\Http\Response;
+use App\Controller;
+
+$router->get('/404', [
+    function($request) {
+        return new Response(200, Controller\Pages::error($request));
+    }
 ]);
 
-\App\Http\Middleware\Queue::setMap([
-    'maintenance' => \App\Http\Middleware\Maintenance::class,
-    'admin-login' => \App\Http\Middleware\AdminLogin::class,
-    'admin-logout' => \App\Http\Middleware\AdminLogout::class
+$router->get('/', [
+    function($request) {
+        return new Response(200, Controller\Posts::getPosts($request));
+    }
 ]);
 
-\App\Http\Middleware\Queue::setDefault(['maintenance']);
+$router->get('/admin', [
+    'middlewares' => ['admin-login'],
+    function($request) {
+        return new Response(200, Controller\Admin::getAdminPanel('Admin', '', 'home'));
+    }
+]);
+
+$router->get('/admin/login', [
+    'middlewares' => ['admin-logout'],
+    function($request) {
+        return new Response(200, Controller\User::getAdminLogin($request));
+    }
+]);
+
+$router->post('/admin/login', [
+    function($request) {
+        return new Response(200, Controller\User::setAdminLogin($request));
+    }
+]);
+
+$router->get('/admin/logout', [
+    'middlewares' => ['admin-login'],
+    function($request) {
+        return new Response(200, Controller\User::setAdminLogout($request));
+    }
+]);
+
+$router->get('/posts/new', [
+    function($request) {
+        return new Response(200, Controller\Posts::getPost($request));
+    }
+]);
+
+$router->post('/posts/new', [
+    function($request) {
+        return new Response(200, Controller\Posts::setPost($request));
+    }
+]);
--- public/index.php
+++ public/index.php
@@ -14,6 +14,6 @@ require APP . '/bootstrap.php';
 
 $router = new Router(URL);
 
-
+require APP . '/routes.php';
 
 $router->dispatch()->sendResponse();
