b6a592439fabb8392862330ce12a4f135239d4d9
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-12 20:52:01 -0400

    Autocommit por GitHub: alterações: add 'app/Core/Database.php'

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -4,73 +4,71 @@ namespace App\Core;
 
 class Database
 {
-  private $table;
-  private $connection;
-  private static $driver    = 'sqlite';
-  private static $path      = DATABASE;
+    private $table;
+    private $connection;
+    private static $driver    = 'sqlite';
+    private static $path      = DATABASE;
 
-  public function __construct($table = null)
-  {
-    $this->table = $table;
-    $this->setConnection();
-  }
-
-  private function create() {
-    $db = new \PDO(self::$driver . ':' . self::$path);
-
-    $sql = file_get_contents('file.sql');
-
-    $qr = $db->exec($sql);
-  }
-
-  private function setConnection()
-  {
-    try{
-      $this->connection = new \PDO(self::$driver . ':' . self::$path);
-      $this->connection->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION);
-    }catch(\PDOException $e){
-      die('ERROR: '.$e->getMessage());
+    public function __construct($table = null)
+    {
+        $this->table = $table;
+        $this->setConnection();
     }
-  }
 
-  public function execute($query,$params = [])
-  {
-    try{
-      $statement = $this->connection->prepare($query);
-      $statement->execute($params);
-      return $statement;
-    }catch(\PDOException $e){
-      die('ERROR: '.$e->getMessage());
+    private function create() {
+        $db = new \PDO(self::$driver . ':' . self::$path);
+        $sql = file_get_contents('file.sql');
+        $db->exec($sql);
     }
-  }
 
-  public function insert($values)
-  {
-    $fields = array_keys($values);
-    $binds  = array_pad([],count($fields),'?');
-    $query = 'INSERT INTO '.$this->table.' ('.implode(',',$fields).') VALUES ('.implode(',',$binds).')';
-    $this->execute($query,array_values($values));
-    return $this->connection->lastInsertId();
-  }
+    private function setConnection()
+    {
+        try {
+            $this->connection = new \PDO(self::$driver . ':' . self::$path);
+            $this->connection->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION);
+        } catch(\PDOException $e) {
+            die('ERROR: '.$e->getMessage());
+        }
+    }
 
-  public function select($where = null, $order = null, $limit = null, $fields = '*'){
-    $where = strlen($where) ? 'WHERE '.$where : '';
-    $order = strlen($order) ? 'ORDER BY '.$order : '';
-    $limit = strlen($limit) ? 'LIMIT '.$limit : '';
-    $query = 'SELECT '.$fields.' FROM '.$this->table.' '.$where.' '.$order.' '.$limit;
-    return $this->execute($query);
-  }
+    public function execute($query,$params = [])
+    {
+        try {
+            $statement = $this->connection->prepare($query);
+            $statement->execute($params);
+            return $statement;
+        } catch(\PDOException $e) {
+            die('ERROR: '.$e->getMessage());
+        }
+    }
 
-  public function update($where,$values){
-    $fields = array_keys($values);
-    $query = 'UPDATE '.$this->table.' SET '.implode('=?,',$fields).'=? WHERE '.$where;
-    $this->execute($query,array_values($values));
-    return true;
-  }
+    public function insert($values)
+    {
+        $fields = array_keys($values);
+        $binds  = array_pad([],count($fields),'?');
+        $query = 'INSERT INTO '.$this->table.' ('.implode(',',$fields).') VALUES ('.implode(',',$binds).')';
+        $this->execute($query,array_values($values));
+        return $this->connection->lastInsertId();
+    }
 
-  public function delete($where){
-    $query = 'DELETE FROM '.$this->table.' WHERE '.$where;
-    $this->execute($query);
-    return true;
-  }
+    public function select($where = null, $order = null, $limit = null, $fields = '*'){
+        $where = strlen($where) ? 'WHERE '.$where : '';
+        $order = strlen($order) ? 'ORDER BY '.$order : '';
+        $limit = strlen($limit) ? 'LIMIT '.$limit : '';
+        $query = 'SELECT '.$fields.' FROM '.$this->table.' '.$where.' '.$order.' '.$limit;
+        return $this->execute($query);
+    }
+
+    public function update($where,$values){
+        $fields = array_keys($values);
+        $query = 'UPDATE '.$this->table.' SET '.implode('=?,',$fields).'=? WHERE '.$where;
+        $this->execute($query,array_values($values));
+        return true;
+    }
+
+    public function delete($where){
+        $query = 'DELETE FROM '.$this->table.' WHERE '.$where;
+        $this->execute($query);
+        return true;
+    }
 }
