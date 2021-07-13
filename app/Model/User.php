3aa9d1a17939397782c5a51b28b9224bd3575f9e
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-12 22:14:01 -0400

    Autocommit por GitHub: alterações: add 'app/Model/User.php'

--- app/Model/User.php
+++ app/Model/User.php
@@ -2,9 +2,9 @@
 
 namespace App\Model;
 
-use \App\Core\Database;
+use App\Core\Database;
 
-class User
+class Post
 {
     public $id;
     public $login;
@@ -13,9 +13,51 @@ class User
     public $fullname;
     public $role;
 
+    public function create()
+    {
+        date_default_timezone_set('America/Sao_Paulo');
+
+        $this->id = (new Database('users'))->insert([
+            'author_id' => $this->author,
+            'title' => $this->title,
+            'description' => $this->description,
+            'image' => $this->image,
+            'likes' => 0
+        ]);
+
+        return true;
+    }
+
+    public static function read($where = null, $order = null, $limit = null, $fields = '*')
+    {
+        return (new Database('users'))->select($where, $order, $limit, $fields);
+    }
+
+    public function update()
+    {
+        date_default_timezone_set('America/Sao_Paulo');
+
+        return (new Database('users'))->update('id = ' . $this->id,[
+            'author_id' => $this->author,
+            'title' => $this->title,
+            'description' => $this->description,
+            'image' => $this->image,
+            'updated' => date('Y-m-d H:i:s', time())
+        ]);
+    }
+
+    public function delete()
+    {
+        return (new Database('users'))->delete('id = ' . $this->id);
+    }
+
+    public static function id($id)
+    {
+        return self::read('id = ' . $id)->fetchObject(self::class);
+    }
+
     public static function getUserByEmail($email)
     {
         return (new Database('users'))->select('email = "' . $email . '"')->fetchObject(self::class);
     }
-
 }
